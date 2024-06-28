fc1f48cd7eb96e220d68647003f7b107fda1b022
ChengXiao686 <xiao.cheng@autra.tech>
2024-06-28 17:46:45 +0800

    add download

---
+++ test/download.py
@@ -0,0 +1,11 @@
+from datasets import load_dataset
+
+ds1 = load_dataset("RLHFlow/iterative-prompt-v1-iter1-20K")
+ds2 = load_dataset("RLHFlow/iterative-prompt-v1-iter2-20K")
+ds3 = load_dataset("RLHFlow/iterative-prompt-v1-iter3-20K")
+ds4 = load_dataset("RLHFlow/iterative-prompt-v1-iter4-20K")
+ds5 = load_dataset("RLHFlow/iterative-prompt-v1-iter5-20K")
+ds6 = load_dataset("RLHFlow/iterative-prompt-v1-iter6-20K")
+ds7 = load_dataset("RLHFlow/iterative-prompt-v1-iter7-20K")
+ds8 = load_dataset("RLHFlow/iterative-prompt-v1-iter8-20K")
+ds9 = load_dataset("RLHFlow/iterative-prompt-v1-iter9-20K")

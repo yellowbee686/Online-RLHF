de0658cc869e76edfcd9900427f324cc3203dbe3
ChengXiao686 <xiao.cheng@autra.tech>
2024-06-28 11:55:48 +0800

    add train_init

---
+++ test/train_init.sh
@@ -0,0 +1,10 @@
+conda create -n rlhflow python=3.10.9
+conda activate rlhflow
+
+git clone https://github.com/huggingface/alignment-handbook.git
+cd ./alignment-handbook/
+git checkout d17fd7cd3b71c6a7bf7af34d8dc73135bb7ea8e9
+pip3 install torch==2.1.2 torchvision torchaudio
+python -m pip install .
+pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.7/flash_attn-2.5.7+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+pip install accelerate==0.27.2

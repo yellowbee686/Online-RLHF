a9d8ddca4b4a14274c62dd2457be2934425a6fe8
ChengXiao686 <xiao.cheng@autra.tech>
2024-06-28 19:23:25 +0800

    run loop

--- run_loop.sh
+++ run_loop.sh
@@ -1,15 +1,15 @@
-source ~/.bashrc
+# source ~/.bashrc
 
 # Initialize Conda environment
-eval "$(conda shell.bash hook)"
+# eval "$(conda shell.bash hook)"
 
 
 # Base paths and settings
-initial_model="TODO: Set initial model path here" 
+initial_model="RLHFlow/LLaMA3-SFT" 
 #"meta-llama/Meta-Llama-3-8B-Instruct"
-base_path="TODO: Set base path here"
+base_path="/mnt/raid5/xc/rlhf_online/dpo"
 #"/home/wx/Iterative-RLHF-dev/test"
-mkdir $base_path
+# mkdir $base_path
 iteration_prefix="Test"
 
 
@@ -26,16 +26,16 @@ run_iteration() {
     conda activate vllm
     bash generation/run_8gpu.sh $model_path
     sleep 60
-    python generation/gen_hf.py --ports 8000 8001 8002 8003 --eos_ids 128009 --tokenizer $initial_model --dataset_name_or_path $jsonl_input --output_dir $json_output --K 8 --temperature 1.0
+    python generation/gen_hf.py --ports 8001 8002 8003 8004 --eos_ids 128009 --tokenizer $initial_model --dataset_name_or_path $jsonl_input --output_dir $json_output --K 8 --temperature 1.0
     pkill -f "python -m vllm.entrypoints.api_server"
-    accelerate launch annotate_data/get_rewards.py --dataset_name_or_path $json_output --output_dir $model_output
+    accelerate launch annotate_data/get_rewards.py --dataset_name_or_path $json_output --output_dir $model_output --K 8
     conda activate rlhflow
-    accelerate launch --config_file ./configs/zero2.yaml dpo_iteration/run_dpo.py --run_name $iteration --output_dir $iteration --model_name_or_path $model_path --ref_model $initial_model --learning_rate 5e-7 --max_steps 1200 --choose_type max_min --train_dir $model_output --eval_dir $model_output --loss_type sigmoid --lr_scheduler_type cosine
+    accelerate launch --config_file ./configs/zero2_test.yaml dpo_iteration/run_dpo.py --run_name $iteration --output_dir $iteration --model_name_or_path $model_path --ref_model $initial_model --learning_rate 5e-7 --max_steps 1200 --choose_type max_min --train_dir $model_output --eval_dir $model_output --loss_type sigmoid --lr_scheduler_type cosine
 }
 
 
 # Main loop for iterations
-for i in {1..3}
+for i in {1..9}
 do
     iteration_name="LLaMA3_iter${i}"
     jsonl_input="RLHFlow/iterative-prompt-v1-iter${i}-20K"

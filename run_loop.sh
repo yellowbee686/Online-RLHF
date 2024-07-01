7a9422d90dbe0226ce6169d85ff85a0511f6a43a
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-01 17:03:46 +0800

    simple train

--- run_loop.sh
+++ run_loop.sh
@@ -1,5 +1,5 @@
 source ~/.bashrc
-export CUDA_VISIBLE_DEVICES='3'
+export CUDA_VISIBLE_DEVICES='2,3'
 # Initialize Conda environment
 eval "$(conda shell.bash hook)"
 
@@ -33,7 +33,7 @@ run_iteration() {
     accelerate launch --config_file ./configs/zero2_single.yaml dpo_iteration/run_dpo.py \
         --run_name $iteration --output_dir $iteration --model_name_or_path $model_path --ref_model $initial_model --learning_rate 5e-7 \
         --max_steps 1200 --choose_type max_min --train_dir $model_output --eval_dir $model_output --loss_type sigmoid --lr_scheduler_type cosine \
-        --len_penalty 0.001 --num_train_epochs 1 --gradient_accumulation_steps 4
+        --len_penalty 0.001 --num_train_epochs 1 --gradient_accumulation_steps 8
 }
 
 

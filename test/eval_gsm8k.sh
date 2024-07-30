0d49f361b06d2955be607321c03baa386b3db2c2
ChengXiao686 <xiao.cheng@autra.tech>
2024-07-30 15:36:48 +0800

    update eval

--- test/eval_gsm8k.sh
+++ test/eval_gsm8k.sh
@@ -1,19 +1,19 @@
-source ~/.bashrc
-export CUDA_VISIBLE_DEVICES='4,5,6,7'
-eval "$(conda shell.bash hook)"
+# source ~/.bashrc
+export CUDA_VISIBLE_DEVICES='2,3'
+# eval "$(conda shell.bash hook)"
 
-# model_path="LLaMA3_iter9"
+model_path="LLaMA3_iter1"
 # model_path="RLHFlow/LLaMA3-SFT"
-model_path="meta-llama/Meta-Llama-3-8B-Instruct"
+# model_path="meta-llama/Meta-Llama-3-8B-Instruct"
 # model_path="/mnt/raid5/xc/rlhf_online/exps/dpo_armo_sigmoid_lc/LLaMA3_iter4"
 jsonl_input="openai/gsm8k"
 
 
 conda activate vllm
-bash generation/run_8gpu.sh $model_path
+bash generation/eval_vllm.sh $model_path
 sleep 60
 python eval/eval_gsm8k.py \
-    --ports 8004 8005 8006 8007 \
+    --ports 8002 8003 \
     --eos_ids 128009 \
     --tokenizer $model_path \
     --dataset_name_or_path $jsonl_input \
